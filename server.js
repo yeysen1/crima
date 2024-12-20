c1e5d2041b06dded09996d08df9a2f3067d9c5fd
yeysen1 <yeysenbahena@gmail.com>
2024-12-19 20:29:30 -0800

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const app = express();
   - angle: first angle (0–90)
   - angle2: second angle (0–90)
   - fire: indicates if FIRE was pressed (2s)
-  - arm: indicates if ARM was pressed (2s)
+  - arm: indicates if ARM was pressed (toggle)
+  - forward: indicates if FORWARD was pressed (toggle)
+  - backwards: indicates if BACKWARDS was pressed (toggle)
+  - 1footup: indicates if 1 Foot Up was pressed (2s)
+  - 1footdown: indicates if 1 Foot Down was pressed (2s)
 */
 let state = {
   up: false,
@@ -19,33 +23,59 @@ let state = {
   angle: 0,
   angle2: 0,
   fire: false,
-  arm: false
+  arm: false,
+  forward: false,
+  backwards: false,
+  '1footup': false,
+  '1footdown': false
 };
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// POST /update: set one direction to true for 2 seconds
-app.post('/update', (req, res) => {
-  const { direction } = req.body;
-  if (!Object.prototype.hasOwnProperty.call(state, direction)) {
-    return res.status(400).json({ error: 'Invalid direction' });
+// Helper function to deactivate mutually exclusive toggle buttons
+function deactivateExclusiveButtons(except) {
+  if (except !== 'forward') state.forward = false;
+  if (except !== 'backwards') state.backwards = false;
+  if (except !== 'arm') state.arm = false;
+}
+
+// POST /forward: Toggle Forward
+app.post('/forward', (req, res) => {
+  if (state.forward) {
+    // If already active, deactivate
+    state.forward = false;
+  } else {
+    // Activate Forward and deactivate others
+    deactivateExclusiveButtons('forward');
+    state.forward = true;
   }
+  res.json(state);
+});
 
-  // Reset all arrow states
-  state.up = false;
-  state.down = false;
-  state.left = false;
-  state.right = false;
+// POST /backwards: Toggle Backwards
+app.post('/backwards', (req, res) => {
+  if (state.backwards) {
+    // If already active, deactivate
+    state.backwards = false;
+  } else {
+    // Activate Backwards and deactivate others
+    deactivateExclusiveButtons('backwards');
+    state.backwards = true;
+  }
+  res.json(state);
+});
 
-  // Set the chosen direction to true
-  state[direction] = true;
-
-  // Reset after 2 seconds
-  setTimeout(() => {
-    state[direction] = false;
-  }, 2000);
-
+// POST /arm: Toggle ARM
+app.post('/arm', (req, res) => {
+  if (state.arm) {
+    // If already active, deactivate
+    state.arm = false;
+  } else {
+    // Activate ARM and deactivate others
+    deactivateExclusiveButtons('arm');
+    state.arm = true;
+  }
   res.json(state);
 });
 
@@ -82,11 +112,20 @@ app.post('/fire', (req, res) => {
   res.json(state);
 });
 
-// POST /arm: set arm = true for 2 seconds
-app.post('/arm', (req, res) => {
-  state.arm = true;
+// POST /1footup: set 1footup = true for 2 seconds
+app.post('/1footup', (req, res) => {
+  state['1footup'] = true;
   setTimeout(() => {
-    state.arm = false;
+    state['1footup'] = false;
+  }, 2000);
+  res.json(state);
+});
+
+// POST /1footdown: set 1footdown = true for 2 seconds
+app.post('/1footdown', (req, res) => {
+  state['1footdown'] = true;
+  setTimeout(() => {
+    state['1footdown'] = false;
   }, 2000);
   res.json(state);
 });
